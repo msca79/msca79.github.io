52c06df340b85a37cab94f1ac7fa8287e14bf54c
Dani Szabolcs Márton <Szabolcs.Dani@atollgroup.eu>
2026-06-19 20:37:53 +0200

    csokonai

--- www/includes/welcome.php
+++ www/includes/welcome.php
@@ -36,3 +36,92 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 </script>
+
+<?php
+$now = time();
+if (isset($_GET['test_time'])) {
+    $testTime = strtotime($_GET['test_time']);
+    if ($testTime !== false) {
+        $now = $testTime;
+    }
+}
+$dayOfWeek = date('N', $now); // 1 (Mon) - 7 (Sun)
+$currentTime = date('H:i', $now);
+
+$schedule = [
+    ['id' => 1, 'start' => '08:00', 'end' => '08:45'],
+    ['id' => 2, 'start' => '09:00', 'end' => '09:45'],
+    ['id' => 3, 'start' => '10:00', 'end' => '10:45'],
+    ['id' => 4, 'start' => '10:55', 'end' => '11:40'],
+    ['id' => 5, 'start' => '11:50', 'end' => '12:35'],
+    ['id' => 6, 'start' => '12:45', 'end' => '13:30'],
+    ['id' => 7, 'start' => '13:40', 'end' => '14:25'],
+];
+
+$statusText = '';
+$subText = '';
+$icon = 'ti-clock';
+
+if ($dayOfWeek > 5) {
+    $statusText = 'Hétvége van';
+    $subText = 'Az iskola zárva tart.';
+} else {
+    $currentLesson = null;
+    $nextLesson = null;
+    $isBreak = false;
+
+    foreach ($schedule as $index => $lesson) {
+        if ($currentTime >= $lesson['start'] && $currentTime < $lesson['end']) {
+            $currentLesson = $lesson;
+            if (isset($schedule[$index + 1])) {
+                $nextLesson = $schedule[$index + 1];
+            }
+            break;
+        }
+        
+        if ($currentTime < $lesson['start']) {
+            $nextLesson = $lesson;
+            $isBreak = true;
+            break;
+        }
+    }
+
+    if ($currentLesson) {
+        $statusText = $currentLesson['id'] . '. óra van';
+        $subText = $currentLesson['start'] . ' - ' . $currentLesson['end'];
+        if ($nextLesson) {
+            $subText .= ' | Következő: ' . $nextLesson['id'] . '. óra (' . $nextLesson['start'] . ')';
+        } else {
+            $subText .= ' | Ez az utolsó óra mára.';
+        }
+        $icon = 'ti-bell-ringing';
+    } elseif ($isBreak) {
+        if ($nextLesson['id'] == 1) {
+            $statusText = 'Tanítás kezdete előtt';
+            $subText = 'Első óra kezdete: ' . $nextLesson['start'];
+        } else {
+            $statusText = 'Szünet van';
+            $subText = 'Következő: ' . $nextLesson['id'] . '. óra (' . $nextLesson['start'] . ')';
+        }
+        $icon = 'ti-coffee';
+    } else {
+        $statusText = 'A tanítás mára véget ért';
+        $subText = 'Holnap találkozunk!';
+        $icon = 'ti-moon';
+    }
+}
+?>
+
+<section class="bell-status">
+    <div class="inner">
+        <div class="status-card">
+            <div class="status-icon">
+                <i class="ti <?= $icon ?>"></i>
+            </div>
+            <div class="status-info">
+                <div class="status-main"><?= $statusText ?></div>
+                <div class="status-sub"><?= $subText ?></div>
+            </div>
+        </div>
+    </div>
+</section>
--- www/iskolankrol/csengetes.php
+++ www/iskolankrol/csengetes.php
@@ -12,9 +12,15 @@
 <main id="page-content">
     <section id="sample-page">
 
-
-Csengetési rend
-
+<pre>
+1. 8:00-8:45
+2. 9:00-9:45
+3. 10:00-10:45
+4. 10:55-11:40
+5. 11:50-12:35
+6. 12:45-13:30
+7. 13:40-14:25
+</pre>
 
     </section>
 </main>
